f8d40a64f08c1388f39543acbef770510c6c1ea2
hikarinessa <nessa_vardalmir@hotmail.com>
2022-12-05 12:51:47 +0100

    Day 5 cleanup

--- advent_2022_hikarinessa/advent_05.py
+++ advent_2022_hikarinessa/advent_05.py
@@ -60,48 +60,36 @@ def parse_input(my_input):
     return crates_dict, moves_list
 
 
-def part1(my_input):
-    crates_dict, moves_list = parse_input(my_input.copy())
-    final_string = ''
+def magic(my_input):
+    crates_dict_one, moves_list = parse_input(my_input.copy())
+    crates_dict_two = crates_dict_one.copy()
+    final_string_one = ''
+    final_string_two = ''
 
     # perform all the moves
     for i in moves_list:
         x_nr = i[0]
         x_from = i[1]
         x_to = i[2]
+        # part 1
         for j in range(0, x_nr):
-            moved = crates_dict[x_from][-1:]
-            crates_dict[x_from] = crates_dict[x_from][:-1]
-            crates_dict[x_to] += moved
+            moved = crates_dict_one[x_from][-1:]
+            crates_dict_one[x_from] = crates_dict_one[x_from][:-1]
+            crates_dict_one[x_to] += moved
+        # part 2
+        moved = crates_dict_two[x_from][-x_nr:]
+        crates_dict_two[x_from] = crates_dict_two[x_from][:-x_nr]
+        crates_dict_two[x_to] += moved
 
-    # collect the final string
-    for i in range(len(crates_dict)):
-        final_string += crates_dict[i+1][-1:]
+    # collect the final strings
+    for i in range(len(crates_dict_one)):
+        final_string_one += crates_dict_one[i+1][-1:]
+        final_string_two += crates_dict_two[i+1][-1:]
 
-    return final_string
-
-
-def part2(my_input):
-    crates_dict, moves_list = parse_input(my_input.copy())
-    final_string = ''
-
-    # perform all the moves
-    for i in moves_list:
-        x_nr = i[0]
-        x_from = i[1]
-        x_to = i[2]
-        moved = crates_dict[x_from][-x_nr:]
-        crates_dict[x_from] = crates_dict[x_from][:-x_nr]
-        crates_dict[x_to] += moved
-
-    # collect the final string
-    for i in range(len(crates_dict)):
-        final_string += crates_dict[i+1][-1:]
-
-    return final_string
+    return final_string_one, final_string_two
 
 
 if __name__ == "__main__":
-    print('Part 1 =', part1(INPUT))  # VWLCWGSDQ
+    print('Part 1 =', magic(INPUT)[0])  # VWLCWGSDQ
     print('*'*30)
-    print('Part 2 =', part2(INPUT))  # TCGLQSLPW
+    print('Part 2 =', magic(INPUT)[1])  # TCGLQSLPW
